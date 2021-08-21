230951aadae36a8724f2a524ef07ac33d43ea0c8
Crushed Ice <tenax92@gmx.de>
2021-08-21 20:04:22 +0200

    Add project files.

---
+++ Changelog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeLogGen
+{
+    public class Version
+    {
+        public string tag { get; set; }
+        public string date { get; set; }
+        public List<string> changes { get; set; }
+
+        public Version(string v, string d, List<string>c)
+        {
+            tag = v;
+            date = d;   
+            changes = c;
+        }
+    }
+
+    public class Changelog
+    {
+        public List<Version> versions { get; set; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeLogGen
+{
+    public partial class Form1 : Form
+    {
+        private Changelog _changelog;
+        private readonly string _logfile = "changelog-generator/data/changelog.json";
+        public Form1()
+        {
+            InitializeComponent();
+
+            Start();
+
+        }
+
+        void Start()
+        {
+            
+            if(File.Exists(_logfile))
+            {
+                _changelog = JsonConvert.DeserializeObject<Changelog>(File.ReadAllText(_logfile));
+                
+            }
+            else
+            {
+                _changelog = new Changelog();
+            }
+
+
+        
+        
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string _version = textBox1.Text ;
+            string _date;
+            List<string> _cmds = new List<string>();
+
+            if (checkBox1.Checked)
+                _date = DateTime.Now.ToShortDateString();
+            else
+                _date = textBox2.Text;
+
+            foreach(string l in richTextBox1.Lines)
+                _cmds.Add(l);
+
+            _changelog.versions.Add(new Version(_version, _date, _cmds));
+
+            _changelog.versions.Reverse();
+
+            File.WriteAllText(_logfile, JsonConvert.SerializeObject(_changelog,Formatting.Indented));
+
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            richTextBox1.Text = "";
+
+        }
+    }
+}
